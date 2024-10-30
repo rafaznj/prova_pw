dac3057be29bde2730dd3ed9cdc6fdec869e634f
rafaznj <rafaelsenabarreto@gmail.com>
2024-10-30 07:57:31 -0300

    Incluido botao e redir.

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="estilo.css" rel="stylesheet">
 </head>
 <body>
     <h1>BERNARDO SILVA MALTEZ E RAFAEL SENA BARRETO SILVA</h1>
     <img src="imagens/pizza.jfif">
+
+    <form action="cadastros.php" method="post">
+        <input type="submit" name="txtCadastros"  class="textbox" value="Cadastros de usuário" />
+    </form>
 </body>
 </html>
